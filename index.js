40a8e083cdf7ba9707078a40501439339c4b48bd
mael02455 <esmaelsanxo435@gmail.com>
2025-05-28 16:57:11 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,27 +13,19 @@ const conexao = mysql.createConnection({
 })
 
 app.post('/centro_treinamento' , (req, res) => {
-    const sessoes = {
-        aluno: req.body.aluno,
-        personal: req.body.personal,
-        tipo_treino: req.body.tipo_treino,
-        data: req.body.data,
-        horario: req.body.horario,
-        observacoes: req.body.observacoes
-    }
-
-    console.log(sessoes)
+    const {aluno, personal, tipo_treino, data, horario, observacao} = req.body
+      
 
     conexao.query(
-        'insert into sessoes (aluno, personal, tipo_treino, data, horario, observacoes) values(?,?,?,?,?,?)',
+        'insert into sessoes (aluno, personal, tipo_treino, data, horario, observacoes, nome_plano, duracao, preco, descricao) values(?,?,?,?,?,?,?,?,?,?)',
         [
-            sessoes.aluno,
-            sessoes.personal,
-            sessoes.tipo_treino,
-            sessoes.data,
-            sessoes.horario,
-            sessoes.observacoes
-
+            aluno,
+            personal,
+            tipo_treino,
+            data,
+            horario,
+            observacao,
+            
         ],
         () => {
             res.status(201).send('sessão agendada com sucesso!')
@@ -49,6 +41,66 @@ app.get('/centro_treinamento', (req, res) => {
         res.status(200).send(results)
     })    
 })
-app.listen(3000, ()=> {
+app.delete(' centro_treinamento', (req, res) => {
+    const { id } = req.params;
+    conexao.query('delete from ssesoes where id = ?', [id], (err, results) => {
+        if (err) {
+            return res.status(500).send('arro ao deletar');
+        }
+        if (results.affectedRows === 0) {
+            return res.status(404).send('sessão não encontrada')
+        }
+        res.status(200).send('ssesão deletada com susseso');
+    });
+});
+app.post('/plano' , (req, res) => {
+    const {nome_plano, duracao, preco, descricao} = req.body
+      
+
+    conexao.query(
+        'insert into plano ( nome_plano, duracao, preco, descricao) values(?,?,?,?',
+        [
+            nome_plano,
+            duracao,
+            preco,
+            descricao,
+            
+            
+        ],
+        () => {
+            res.status(201).send('plano contratado com  com sucesso!')
+        }
+        
+    )
+})
+
+app.get('/plano', (req, res )=> {
+    conexao.query('select * from plano', (err, results) => {
+        if (err) {
+            return res.status(500).send("erro ao buscar plano")
+        }
+        res.status(200).send(results)
+    })  
+})
+
+app.put('plano/:id',(req, res) => {
+    const {id} = req.params;
+    const{nome, preco } = req.body;
+
+    const   query ='update plano set nome = ?, preco  = ? where id = ?'
+    conexao.query(query,[nome, preco, id ],(err, results) =>{
+        if (err) {
+            return res.status(500).send('erro ao atualizar');
+            if (results.affectedRows === 0) {
+                return res.status(404).send('plano nao encontado');
+            
+            }
+            res.send('produto atualizado com susseso')
+        }
+
+    })
+})
+
+app.listen(3000, () => {
     console.log("Servidor backend rodando em http://localhost:3000")
 })
